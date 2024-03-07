1724c486298b57309e46c9573ed6335014461a25
isaias sanchez <isaias.shernandez@alumnos.udg.mx>
2024-03-07 16:33:21 -0600

    Tipado estatico

---
+++ hello-world.js
@@ -0,0 +1,11 @@
+//Imprime un mensaje
+console.log('Hola mundo');
+/*Imprime u mensaje que ha sido declarado de tipo texto*/
+var text = 'hola' + ' como estas';
+//Solo acepta verdadero o falso y en este caso solo estaremos aceptando: falso.
+var bandera = false;
+//Aqui declararemos valores constantes y sus valores no cambian.
+var listaNumeros = [12, 43, 2, 23, 4];
+var cualquiera = false;
+$.
+    console.log(text);
--- hello-world.ts
+++ hello-world.ts
@@ -1,1 +1,13 @@
-console.log('Holas mundos')
+//Imprime un mensaje
+console.log('Hola mundo')
+/*Imprime u mensaje que ha sido declarado de tipo texto*/
+let text: string = 'hola'+' como estas'
+//Solo acepta verdadero o falso y en este caso solo estaremos aceptando: falso.
+let bandera: boolean = false
+//Aqui declararemos valores constantes y sus valores no cambian.
+const listaNumeros: number[] =  [12,43,2,23,4]
+
+// no se recomienda utilizar salvo sea muy necesario
+const cualquiera: any = false
+console.log(text)
+
