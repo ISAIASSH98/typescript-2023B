db2d40ded1a7f504ae32d994bfdc8257603658a3
isaias sanchez <isaias.shernandez@alumnos.udg.mx>
2024-03-16 16:15:38 -0600

    Declarando variables globales ("var") y variables de tipo locales ("let")

---
+++ src/sesion-2/let.ts
@@ -0,0 +1,28 @@
+let index: number = 0
+if(index == 0){
+    let index: number = 9
+    console.log(`index: ${index}`)
+}
+console.log(`index: ${index}`)
+class miClase {
+    private nombre: string = ''
+    print() {
+        this.print()
+        
+    }
+}
+
+
+
+
+
+
+/*
+si trbajamos con ts crudo, posiblemente coincidamos con nombre  de variables reservadas por TS
+para evitar esto podemos agregar la siguiente configuraciój: 
+"moduleDetection": "force"
+o agregando export {} al inicio del archivo
+o simplemente cambiando el nombre de la variable.
+*/
+//let name = 'isaias'
+ 
