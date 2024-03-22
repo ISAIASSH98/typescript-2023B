656d9dc9cb80449c2194c4fb0b643a1e51a2f307
isaias sanchez <isaias.shernandez@alumnos.udg.mx>
2024-03-22 13:35:35 -0600

    propagacion 2 de objetos

--- src/sesion-3/propagacion.ts
+++ src/sesion-3/propagacion.ts
@@ -4,10 +4,16 @@ let secondObj: object = {...firstObjt}
 
 let objName : object = {name: 'juan', id: 34}
 let objId: object = {id: 2}
-let obj3= {desc: 'test jsndkjdsnskjsdj'}
+//let obj3: object = {...objId, ...objName}
 
 /* La propagacion de objetos , nos sirve para evitar asignar
 la asignacion de valores explicitamente */
-obj3  = {desc: obj3.desc, ...objId}
+let obj3  = {...objName, ...objId}
 
 console.log(obj3)
+
+let arreglo1 = [1,2,3]
+let arrelgo2 = [4,5,6]
+
+let arreglo3 = [...arreglo1, ...arrelgo2]  
+console.log(arreglo3)
